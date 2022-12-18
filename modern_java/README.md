262c13cba2bce794a64369b286f1b289bdcc33a6
한광수 <hankk20@gmail.com>
2022-12-18 22:03:39 +0900

    CompletableFuture 정리중

--- modern_java/README.md
+++ modern_java/README.md
@@ -46,6 +46,9 @@
 ### 16.1.1 Future 형식 API
 
 - Future 제한
-  - [SimpleFuture](/modern_java/src/hankk20/modern_java/reactive/future/SimpleFuture.java) 
+  - [SimpleFuture](/modern_java/src/hankk20/modern_java/reactive/future/SimpleFuture.java)
   - Future.get()으로 결과를 기다려야 한다.
     ![2022-12-17 23 57 38](https://user-images.githubusercontent.com/60081600/208248073-6759a6e7-94b4-41e6-9a08-1ab82254b3c2.png)
+- CompletableFuture
+  - 오류처리를 하지 않으면 get() 호출시점에서 무한으로 대기 한다.
+    - Complatablefuture.completeExceptionally 를 사용하자.
---
+++ modern_java/src/hankk20/modern_java/reactive/future/Shop.java
@@ -0,0 +1,74 @@
+package hankk20.modern_java.reactive.future;
+
+import java.util.Random;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+
+public class Shop {
+    private final Random random = new Random();
+
+    public double getPrice(String product){
+        return calculatePrice(product);
+    }
+
+    public Future<Double> getPriceAsyncSupply(String product){
+        return CompletableFuture.supplyAsync(() -> calculatePrice(product));
+    }
+    public Future<Double> getPriceAsync(String product){
+        CompletableFuture<Double> futurePrice = new CompletableFuture<>();
+        new Thread(() -> {
+            try {
+                double v = calculatePrice(product);
+                futurePrice.complete(v);
+            }catch (Exception e){
+                futurePrice.obtrudeException(e);
+               // futurePrice.completeExceptionally(e);
+            }
+        }).start();
+        return futurePrice;
+    }
+
+    public double calculatePrice(String product){
+        delay();
+        return random.nextDouble() * product.charAt(0) + product.charAt(1);
+    }
+
+    public static void delay(){
+        try {
+            Thread.sleep(1000L);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 블록방식이랑 똑같은데
+     * @param args
+     */
+    public static void main(String[] args) {
+        Shop shop = new Shop();
+        long start = System.nanoTime();
+
+        Future<Double> future = shop.getPriceAsync(null);
+
+        long invocationTime = (System.nanoTime() - start) / 1_000_000;
+        System.out.println("return "+invocationTime+" mesecs");
+
+        other();
+
+        try {
+            double price = future.get();
+            System.out.printf("Price is %.2f%n", price);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        long ret = (System.nanoTime() - start) / 1_000_000;
+        System.out.println("return after "+ret+" mesecs");
+
+    }
+
+    public static void other(){
+        System.out.println("other process");
+    }
+}
